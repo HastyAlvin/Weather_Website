aab6a19b88d35778b66aefd772ef37b72b85b08a
Hasty Alvin <quangthang0311@gmail.com>
2025-01-09 15:41:08 +0700

    ok

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,7 +1,22 @@
+import { AppBar, Toolbar, IconButton, Typography, Button } from "@mui/material";
+
 export default function Header() {
     return (
-        <header>
-            <h1>Header</h1>
-        </header>
+        <AppBar position="static" sx>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     );
 }
